47c0a62de71ef0bedd94af3d2095717ccf55d86a
Jess Portnoy <kernel01@gmail.com>
2015-01-16 20:34:37 +0200

    first commit.

---
+++ php5/TestCode/KalturaTestConfiguration.php
@@ -0,0 +1,36 @@
+<?php
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2011  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+class KalturaTestConfiguration
+{
+	const PARTNER_ID = 101;
+	const SECRET = "6aa758517fc892cf082185208a203116";
+	const ADMIN_SECRET = "0567e52a030ebc2ccb73b386aa1800e0";
+	const SERVICE_URL = "http://54.159.220.35/";
+	const UPLOAD_FILE = "DemoVideo.flv";	
+}
