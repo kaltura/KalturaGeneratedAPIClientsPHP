d93d7ff9d1b06668004400d6ae6db5ae37485b14
root <root@ce-front0.dev.kaltura.com>
2015-03-11 08:38:07 -0400

    New clientlib ver

--- php5/KalturaClientBase.php
+++ php5/KalturaClientBase.php
@@ -1164,7 +1164,7 @@ class KalturaConfiguration
 	public $serviceUrl    				= "http://www.kaltura.com/";
 	public $partnerId    				= null;
 	public $format        				= 3;
-	public $clientTag 	  				= "php5:15-03-09";
+	public $clientTag 	  				= "php5:15-03-11";
 	public $curlTimeout   				= 120;
 	public $userAgent					= '';
 	public $startZendDebuggerSession 	= false;
