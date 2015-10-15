f3a9f3b43ca924a875e949fedfaa12cab2c25576
Kaltura CSI <community@kaltura.com>
2015-10-15 12:56:49 -0400

    New clientlib ver

--- KalturaEnums.php
+++ KalturaEnums.php
@@ -1339,6 +1339,7 @@ class KalturaAssetType extends KalturaEnumBase
 	const PDF = "document.PDF";
 	const SWF = "document.SWF";
 	const TIMED_THUMB_ASSET = "thumbCuePoint.timedThumb";
+	const TRANSCRIPT = "transcript.Transcript";
 	const FLAVOR = "1";
 	const THUMBNAIL = "2";
 	const LIVE = "3";
