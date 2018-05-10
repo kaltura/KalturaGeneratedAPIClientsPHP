2d7f07f2824e15dd6ba1ac32a666cbd179d8995d
Kaltura CSI <community@kaltura.com>
2018-05-10 03:42:25 -0400

    New clientlib ver

--- KalturaEnums.php
+++ KalturaEnums.php
@@ -785,6 +785,7 @@ class KalturaSchedulerStatusType extends KalturaEnumBase
 	const RUNNING_BATCHES_DISC_IO = 5;
 	const RUNNING_BATCHES_DISC_SPACE = 6;
 	const RUNNING_BATCHES_IS_RUNNING = 7;
+	const RUNNING_BATCHES_LAST_EXECUTION_TIME = 8;
 }
 
 /**
