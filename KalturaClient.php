fa237f082aae99da4c2b0a5a0724a2e44e2a5e2e
Kaltura CSI <community@kaltura.com>
2018-10-23 02:36:59 -0400

    New clientlib ver

--- KalturaClient.php
+++ KalturaClient.php
@@ -9580,7 +9580,7 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:18-10-22');
+		$this->setClientTag('php5:18-10-23');
 		$this->setApiVersion('14.7.0');
 		
 		$this->accessControlProfile = new KalturaAccessControlProfileService($this);
