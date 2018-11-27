2512097ddd55fb57409e481a276b9c370dac1da7
Kaltura CSI <community@kaltura.com>
2018-11-27 02:48:52 -0500

    New clientlib ver

--- KalturaEnums.php
+++ KalturaEnums.php
@@ -4564,6 +4564,8 @@ class KalturaReportType extends KalturaEnumBase
 	const CITIES = "30";
 	const OPERATING_SYSTEM_FAMILIES = "32";
 	const BROWSERS_FAMILIES = "33";
+	const USER_ENGAGEMENT_TIMELINE = "34";
+	const UNIQUE_USERS_PLAY = "35";
 	const PARTNER_USAGE = "201";
 }
 
